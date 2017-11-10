1e226d6edf6b4a42cb522c7ec9c2aebbb284901d
Shou Ya <shouya@users.noreply.github.com>
2017-11-10 15:33:13 +0800

    fix a bug that selecting model does not work for docsim

--- algorithm/docsim.py
+++ algorithm/docsim.py
@@ -2,12 +2,12 @@ from . import tfidf
 import math
 
 def docsim(doc_a, doc_b, model='default'):
-  vec_a = doc_to_vec(doc_a)
-  vec_b = doc_to_vec(doc_b)
+  vec_a = doc_to_vec(doc_a, model)
+  vec_b = doc_to_vec(doc_b, model)
   return vec_sim(vec_a, vec_b)
 
-def doc_to_vec(doc):
-  keyed_vec = dict(tfidf(doc, model='default'))
+def doc_to_vec(doc, model):
+  keyed_vec = dict(tfidf(doc, model))
   norm = math.sqrt(sum([v * v for v in keyed_vec.values()]))
   return { k: v / norm for k, v in keyed_vec.items() }
 
