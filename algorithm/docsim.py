f990c2799103d0d7d11aa067884ddb3566cfcf38
Shou Ya <shouya@users.noreply.github.com>
2017-11-23 19:11:11 +0800

    support 1-m docsim and lru_caching

--- algorithm/docsim.py
+++ algorithm/docsim.py
@@ -1,17 +1,28 @@
 from . import tfidf
 import math
+from functools import lru_cache
+
+class NamedVec(dict):
+  def __hash__(self):
+    return hash(frozenset(self.items()))
 
 def docsim(doc_a, doc_b, model='default'):
   vec_a = doc_to_vec(doc_a, model)
   vec_b = doc_to_vec(doc_b, model)
   return vec_sim(vec_a, vec_b)
 
+def docsim_1ton(one, many, model='default'):
+  vec_one = doc_to_vec(one, model)
+  vec_many = [doc_to_vec(x, model) for x in many]
+  return [vec_sim(vec_one, x) for x in vec_many]
+
+@lru_cache(maxsize=1024)
 def doc_to_vec(doc, model):
   keyed_vec = dict(tfidf(doc, model))
   norm = math.sqrt(sum([v * v for v in keyed_vec.values()]))
-  return { k: v / norm for k, v in keyed_vec.items() }
+  return NamedVec({ k: v / norm for k, v in keyed_vec.items() })
 
+@lru_cache(maxsize=1024)
 def vec_sim(a, b):
   intersection = set(a.keys()) & set(b.keys())
   return sum([a[k] * b[k] for k in intersection])
-
--- service.py
+++ service.py
@@ -13,6 +13,12 @@ async def handle_docsim(req):
   model = req.query.get('model', 'default')
   return algorithm.docsim.docsim(a, b, model=model)
 
+@routes.post('/docsim_1ton')
+@utils.render_json
+async def handle_docsim_1ton(req):
+  json = await req.json()
+  model = json['model']
+  return algorithm.docsim.docsim_1ton(json['one'], json['many'], model=model)
 
 app = web.Application()
 app.router.add_routes(routes)
@@ -23,5 +29,3 @@ logger.addHandler(logging.StreamHandler())
 app.make_handler(access_log=logger)
 
 web.run_app(app, port=8080, access_log=logger)
-
-
